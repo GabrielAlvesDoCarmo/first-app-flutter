8256876b78553c6bcf95b57ec90e02f83695c7bc
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2025-04-01 15:24:56 -0300

    fix(firebase):Included firebase and change version ndk android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fisrt_app_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+// import 'firebase_options.dart';'
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 

360c9ea0320f8b5015201467307775c3972b0a41
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2025-04-01 01:46:03 -0300

    chore(init):First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fisrt_app_flutter/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Container(
+        color: Colors.blue,
+        child: SafeArea(
+          child: LoginScreen(),
+        ),
+      ),
+    );
+  }
+}
